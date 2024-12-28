1ded6d69572ccc36881de3fdd1b83edf8356a770
Nguyễn Văn Long <longnv@langtech.vn>
2024-12-28 12:08:24 +0700

    maintain

--- app/Services/ArtificialIntelligence/ChatGPTPromptSevice.php
+++ app/Services/ArtificialIntelligence/ChatGPTPromptSevice.php
@@ -18,15 +18,8 @@ class ChatGPTPromptSevice extends BaseService
     public function chat($options)
     {
         try {
-            $language = [
-                'vi' => 'Tiếng Việt',
-                'en' => 'Tiếng Anh',
-            ];
             $client = new Client();
             $apiKey = env(key: 'OPENAI_API_KEY');
-            $botName = $options['bot_name'] ?? 'Bot Chat';
-            $requirement = $options['bot_description'] ? " và bạn chỉ được trả lời các câu hỏi liên quan đến chủ đề màu sắc, nếu câu hỏi không liên quan đến chủ đề màu sắc thì trả lời :I'm sorry, I can only answer questions related to topic" : "";
-            $content = "Bạn tên là ".$botName.". Hãy trả lời các câu hỏi bằng ".$language[$options['language']]." ngắn gọn, dễ hiểu, chỉ được trả lời bằng ".$language[$options['language']]." cho dù câu hỏi có là bất kỳ ngôn ngữ nào khác";
             
             $response = $client->post('https://api.openai.com/v1/chat/completions', [
                 'headers' => [
@@ -34,19 +27,13 @@ class ChatGPTPromptSevice extends BaseService
                     'Authorization' => 'Bearer ' . $apiKey,
                 ],
                 'json' => [
-                    'model' => 'gpt-3.5-turbo',
-                    'messages' => [
-                        [
-                            "role" => "system",
-                            "content" => $content
-                        ],
-                        [
-                            "role" => "user",
-                            "content" => $options['prompt'] ?? 'Kết thúc' // Nội dung do người dùng nhập vào
-                        ]
-                    ],
-                    'max_tokens' => 150,
-                    'temperature' => 0.7
+                    'model' => $options['model'],
+                    'messages' => $options['messages'],
+                    'max_tokens' => 250,
+                    'temperature' => 0.9,
+                    'frequency_penalty' => 1.6,
+                    'presence_penalty' => 1.9,
+                    'stop' => ["Kết thúc"],
                 ],
             ]);
 
--- app/Services/ChatMessage/StoreByTextService.php
+++ app/Services/ChatMessage/StoreByTextService.php
@@ -36,20 +36,48 @@ class StoreByTextService extends BaseService
 
     public function store($request)
     {
+        $messages = [];
+        $chatRoom = ChatRoom::findOrFail($request['chat_room_id']);
+        $language = [
+            'vi' => 'Tiếng Việt',
+            'en' => 'Tiếng Anh',
+        ];
 
+        $botName = $chatRoom->bot_name ?? 'Bot Chat';
+        $languageOutput =  $chatRoom->language && isset($language[$chatRoom->language]) ? $language[$chatRoom->language] : $language['en'];
+        $requirements = $chatRoom->bot_description ? "" .$chatRoom->bot_description. ", chữa lỗi chính tả, ngữ pháp một cách đầy đủ và chi tiết cho câu trả lời
+        và có thể đưa ra các câu trả lời hợp lý hơn. chỉ được phép trả lời các câu hỏi liên quan đến chủ đề trên" : "";
+        
+        $content = "Bạn tên là ".$botName.". ".$requirements.".
+        Hãy giao tiếp bằng ".$languageOutput." ngắn gọn, dễ hiểu, 
+        chỉ được trả lời bằng ".$languageOutput." 
+        cho dù câu hỏi có là bất kỳ ngôn ngữ nào khác";
+        
+        $messages[] = [
+            'role' => 'developer',
+            'content' => $content,
+        ];
+        
         $this->chatMessageModel->create([
-            'content' => $request['text'],
+            'content' => $request['text'] ?? 'End',
             'chat_room_id' => $request['chat_room_id'],
             'role' => 'user',
             'user_id' => auth(ConstantService::AUTH_USER)->user()->id,
             'audio' => isset($request['audio']) ? $request['audio'] : null,
         ]);
-        
 
-        $chatRoom = ChatRoom::findOrFail($request['chat_room_id']);
+        $historyMessage = $this->chatMessageModel->where('chat_room_id', $request['chat_room_id'])->orderBy('created_at', 'asc')->get(['content', 'role'])->toArray();
+        foreach($historyMessage as $message)
+        {
+            $messages[] = [
+                'role' => $message['role'],
+                'content' => $message['content'],
+            ];
+        }
+
         $chatGPTOptions = [
-            'language' => $chatRoom->language ?? 'vi',
-            'prompt' => $request['text'],
+            'model' => $chatRoom->chat_gpt_model ?? 'gpt-3.5-turbo-0125',
+            'messages' => $messages,
             'bot_name' => $chatRoom->bot_name,
             'bot_description' => $chatRoom->bot_description,
         ];
@@ -63,7 +91,6 @@ class StoreByTextService extends BaseService
 
         $chatGPTTranstionResult = $this->chatGPT->translation($translationOptions);
 
-
         $textToSpeechOptions = [
             'model' => $chatRoom->text_to_speech_model,
             'voice' => $chatRoom->voice_model,
@@ -76,7 +103,7 @@ class StoreByTextService extends BaseService
         $result = $this->chatMessageModel->create([
             'content' => $chatGPTResult,
             'chat_room_id' => $request['chat_room_id'],
-            'role' => 'system',
+            'role' => 'assistant',
             'user_id' => auth(ConstantService::AUTH_USER)->user()->id,
             'audio' => $textToSpeechResult,
             'translation' => $chatGPTTranstionResult,
